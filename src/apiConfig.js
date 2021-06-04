5d28a146521074acbca9f924c9f625aee8a8a49f
dzemsky1 <dzemsky1@gmail.com>
2021-06-04 15:47:09 -0400

    Fixed api

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://mysterious-basin-30873.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
